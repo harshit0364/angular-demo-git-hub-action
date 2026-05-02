92ed435cee91bb9f50468c99892ff840834272a6
Harshit Goyal <46104287+harshit0364@users.noreply.github.com>
2026-05-02 19:24:42 +0530

    adding

---
+++ src/app/app.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { App } from './app';
+
+describe('App', () => {
+  let component: App;
+  let fixture: ComponentFixture<App>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have title signal', () => {
+    expect(component['title']()).toBe('my-project123');
+  });
+});
